1c6f36754ccb3381d7f01c6933aa85257d543c25
Prakhar Dubey <prakhardubey2002@gmail.com>
2024-11-27 11:40:57 +0530

    front layout added

---
+++ app/(front)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <div className='text-2xl py-2' >
+      Latest Products
+    </div>
+  )
+}
+
+export default Home

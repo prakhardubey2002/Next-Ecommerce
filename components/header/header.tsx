1394ccbf6b0cd53aa65ac98220f8be2fe8c6c700
Prakhar Dubey <prakhardubey2002@gmail.com>
2024-11-27 12:16:31 +0530

    routes added

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,17 +1,27 @@
 import Link from 'next/link'
 import React from 'react'
-
-
-
+import {HOME,CART,SIGNIN} from "../../routes/routes"
 function Header() {
   return (
-   <div>
-    <nav className='navbar justify-between bg-base-300' >
-    <Link href="/" className='btn btn-link'  >
-      Next Amazona V2
-    </Link>
-    </nav>
-   </div>
+    <div>
+      <nav className="navbar justify-between bg-base-300">
+        <Link href={HOME} className="btn btn-ghost text-lg">
+          Next Amazona V2
+        </Link>
+        <ul className="flex">
+          <li>
+            <Link href={CART} className="btn btn-ghost rounded-btn ">
+              Cart
+            </Link>
+          </li>
+          <li>
+            <Link href={SIGNIN} className="btn btn-ghost rounded-btn ">
+              Sign in
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
